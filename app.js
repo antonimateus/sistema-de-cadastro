521cdd1c4c62c2e5980f4e948ae1de1111d7be13
antonimateus <127150996+antonimateus@users.noreply.github.com>
2023-05-30 11:26:04 -0300

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao esta se conectando ao banco de dados")
 // mais um console log:
-console.log("o paulo feis esse commit sem voce ver na aula")
+console.log("o paulo fez esse commit sem voce ver na aula")
 console.log("enviando para o github")
